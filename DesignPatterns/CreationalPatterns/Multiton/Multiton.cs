f7c0279ff6c0b2dfdc907275619f93f0cac7693b
ahron levi <ahron.levi@zerto.com>
2016-01-25 17:36:33 +0200

    multiton

---
+++ DesignPatterns/CreationalPatterns/Multiton/Multiton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns.Multiton
+{
+    public class Multiton
+    {
+        public string Key { get; private set; }
+
+        private Multiton()
+        {
+        }
+
+        private Multiton(string key):this()
+        {
+            Key = key;
+        }
+
+        private static Dictionary<string, Multiton> _instances;
+
+        public static Multiton GetInstance(string key)
+        {
+            if (!_instances.ContainsKey(key))
+            {
+                _instances[key] = new Multiton(key);
+            }
+            return _instances[key];
+        }
+
+        static Multiton()
+        {
+            _instances = new Dictionary<string, Multiton>();
+        }
+    }
+}
---
+++ UnitTests.DesignPatterns/CreationalPatterns/MultiTonTests.cs
@@ -0,0 +1,42 @@
+﻿using DesignPatterns.CreationalPatterns.Multiton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.DesignPatterns.CreationalPatterns
+{
+    public class MultiTonTests
+    {
+        [Fact]
+        public void WhenGetInstance_ShouldReturnInstanceNotNull()
+        {
+            //arrange act
+            Multiton sut = Multiton.GetInstance("first");
+            //assert
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void WhenGetInstance_WithTheSameKey_ShouldReturnTheSameinstance()
+        {
+            //arrange act
+            Multiton first = Multiton.GetInstance("first");
+            Multiton sec = Multiton.GetInstance("first");
+            //assert
+            Assert.Equal(first, sec);
+        }
+
+        [Fact]
+        public void WhenGetInstance_WithTheDiffrentKey_ShouldReturnTheDifferentinstances()
+        {
+            //arrange act
+            Multiton first = Multiton.GetInstance("first");
+            Multiton sec = Multiton.GetInstance("sec");
+            //assert
+            Assert.NotEqual(first, sec);
+        }
+    }
+}
